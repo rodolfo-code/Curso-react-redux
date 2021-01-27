a7f04dc1baaceda671d82d2789a11b1b218a5c97
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-27 03:35:35 -0300

    primeiro componente

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+import Primeiro from './components/basicos/primeiro'
+
+ReactDOM.render(
+    <div>
+      <Primeiro />
+    </div>,
+    document.getElementById('root')
+)
