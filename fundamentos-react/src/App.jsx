157457c69afa73ca441d1e5e25dd1e75a8253801
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-27 14:04:35 -0300

    criando cards e colocando conteudo dentro

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -3,21 +3,28 @@ import React from 'react';
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro';
 import Aleatorio from './components/basicos/Aleatorio';
+import Card from './components/layout/Card';
 
 export default function App(props) {
   return (
     <div id='app'>
-      <h1>Fundamentos React</h1>
-      <Primeiro />
-      <ComParametro 
-        titulo='Segundo Componente' 
-        aluno='Rodolfo'
-        nota={ 9.8 }
-      />
-      <Aleatorio 
-        min={ 2 }
-        max={ 18 }
-      />
+      <h1 className='title-header'>Fundamentos React</h1>
+      <Card titulo='Desafio Aleatório'>
+        <Aleatorio 
+          min={ 2 }
+          max={ 60 }
+        />
+      </Card>
+      <Card className='primeiro' titulo='Primeiro Componente'>
+        <Primeiro />        
+      </Card>
+      <Card titulo='Situação do Aluno'>
+        <ComParametro 
+          titulo='Segundo Componente' 
+          aluno='Rodolfo'
+          nota={ 9.8 }
+        />      
+      </Card>
     </div>
   )
 }
--- fundamentos-react/src/components/basicos/Aleatorio.jsx
+++ fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Aleatorio = (props) => {
   const { min, max } = props
-  const rand = Math.ceil(Math.random() * (max - min) + min)
+  const rand = parseInt(Math.random() * (max - min) + min)
   return ( 
     <>
+      <div>Valor aleatório</div>
       <h1>{ rand }</h1>
     </>
    );
