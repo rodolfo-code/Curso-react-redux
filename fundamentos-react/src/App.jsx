cb6e16dce3fc54af8e4954c73ca694923e5c7556
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-28 00:28:11 -0300

    estilizaçao de cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,30 +1,38 @@
+import './App.css'
 import React from 'react';
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro';
 import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
+import Fragmento from './components/basicos/Fragmento';
 
 export default function App(props) {
   return (
-    <div id='app'>
+    <div className='app'>
       <h1 className='title-header'>Fundamentos React</h1>
-      <Card titulo='Desafio Aleatório'>
-        <Aleatorio 
-          min={ 2 }
-          max={ 60 }
-        />
-      </Card>
-      <Card className='primeiro' titulo='Primeiro Componente'>
-        <Primeiro />        
-      </Card>
-      <Card titulo='Situação do Aluno'>
-        <ComParametro 
-          titulo='Segundo Componente' 
-          aluno='Rodolfo'
-          nota={ 9.8 }
-        />      
-      </Card>
+      <div className='cards'>
+        <Card titulo='Desafio Aleatório'>
+          <Aleatorio 
+            min={ 2 }
+            max={ 60 }
+          />
+        </Card>
+        <Card className='primeiro' titulo='Primeiro Componente'>
+          <Primeiro />        
+        </Card>
+        <Card className='fragmento' titulo='Fragmento'>
+          <Fragmento />
+        </Card>
+        <Card titulo='Situação do Aluno'>
+          <ComParametro 
+            titulo='Segundo Componente' 
+            aluno='Rodolfo'
+            nota={ 9.8 }
+          />      
+        </Card>
+      </div>
+
     </div>
   )
 }
---
+++ fundamentos-react/src/components/basicos/Fragmento.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Fragmento = (props) => {
+  return ( 
+    <>
+      <div>Cuidado com esse erro</div>
+      
+    </>
+   );
+}
+ 
+export default Fragmento;
