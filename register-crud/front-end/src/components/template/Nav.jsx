8bf2d3844bddad4d5d5335e4b637d75851129769
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-08 23:34:55 -0300

    estilizaçao Nav

--- register-crud/front-end/src/components/template/Nav.jsx
+++ register-crud/front-end/src/components/template/Nav.jsx
@@ -2,7 +2,21 @@ import "./Nav.css";
 import React from "react";
 
 function Nav() {
-  return <aside className="menu-area">Navegação</aside>;
+  return (
+    <aside className="menu-area">
+      <nav className="menu">
+        {/* Refatorar com componente */}
+        <a href="#/">
+          <i className="fa fa-home"></i>
+          Início
+        </a>
+        <a href="#/users">
+          <i className="fa fa-users"></i>
+          Usuários
+        </a>
+      </nav>
+    </aside>
+  );
 }
 
 export default Nav;
